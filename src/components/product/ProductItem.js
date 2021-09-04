ab390217f7110b6b0d4c802d9255f3045972da4e
Amin <amin.xare@outlook.com>
2021-09-04 23:39:21 +0430

    showing correct product

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,19 +1,13 @@
 import React from "react";
 import classes from "./ProductItem.module.css";
 
-const ProductItem = () => {
+const ProductItem = ({product:{category,description,id,image,price,title}}) => {
   return (
     <div className={classes.product}>
-      <img className={classes["product__img"]} src={null} alt='product image' />
-      <p className={classes["product__title"]}>name</p>
-      <p className={classes["product__description"]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum enim
-        veniam vitae. Possimus ipsum doloremque soluta at aspernatur voluptate
-        debitis, ducimus ut non labore dolorem rem, repellendus natus, corporis
-        mollitia!
-      </p>
+      <img className={classes["product__img"]} src={image} alt={title} />
+      <p className={classes["product__title"]}>{title}</p>
       <div className={classes["product__buy"]}>
-        <span className={classes['product__cost']}>10.5$</span>
+        <span className={classes['product__cost']}>{price}$</span>
         <button className={classes["product__btn"]}>Buy</button>
       </div>
     </div>
--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { productContext } from '../../context/productContext';
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = () => {
+    const {products}=useContext(productContext);
+    console.log(products)
+
     return (
         <div className={classes.products}>
-            <ProductItem />
+            {products.map(product=><ProductItem key={product.id} product={product}/>)}
         </div>
     )
 }
