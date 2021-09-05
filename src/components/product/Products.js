eb84c16bbf4f99b98445af0903c75cc16c753d5a
Amin <amin.xare@outlook.com>
2021-09-05 14:39:41 +0430

    page numbers

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,17 +1,26 @@
-import React, { useContext } from 'react';
-import { productContext } from '../../context/productContext';
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import React, { useContext } from "react";
+import { productContext } from "../../context/productContext";
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
 const Products = () => {
-    const {products}=useContext(productContext);
-    console.log(products)
+  const { products, isLoading,page,lastPage,handlePageBack,handlePageNext } = useContext(productContext);
+  console.log(lastPage);
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
+    <div className={classes.container}>
+      <div className={classes.products}>
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+      <div className={classes.btns}>
+        <button onClick={handlePageBack} className={`${classes.btn} ${page === 1 ? classes.disable:''}`}>Back</button>
+        <button onClick={handlePageNext} className={`${classes.btn} ${page === lastPage ? classes.disable:''}`}>Next</button>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className={classes.products}>
-            {products.map(product=><ProductItem key={product.id} product={product}/>)}
-        </div>
-    )
-}
-
-export default Products
+export default Products;
