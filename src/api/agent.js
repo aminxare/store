12f859490c7fb7d6e17f4cda48128896bcd26be8
aminxare <amin.xare@outlook.com>
2021-09-11 16:14:14 +0430

    category get agent

--- src/api/agent.js
+++ src/api/agent.js
@@ -2,18 +2,24 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://fakestoreapi.com";
 
-const responseBody = (res) => res.data;
+const responseBody = res => res.data;
 const request = {
-  get: (url) => axios.get(url).then(responseBody),
+  get: url => axios.get(url).then(responseBody),
 };
 
 const products = {
   list: () => request.get("/products"),
-  getProduct: (id)=>request.get(`/products/${id}`),
+  getProduct: id => request.get(`/products/${id}`),
+};
+
+const categories = {
+  getCategory: name => request.get(`/products/category/${name}`),
+  getCategoryList: () => request.get("/products/categories"),
 };
 
 const agent = {
   products,
+  categories,
 };
 
 export default agent;
